402d1aedfb4f22614cb9250b80f7a6eae9c67164
root <root@vps655079.ovh.net>
2020-03-22 12:17:45 +0100

    Add new action controller + view. Commit before add KnpSnappyBundle

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use App\Form\CertificateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="homepage")
+     * @param Request $request
+     * @param Session $session
+     * @return Response
      */
-    public function index(Request $request)
+    public function index(Request $request, Session $session)
     {
         $form = $this->createForm(CertificateType::class);
 
@@ -20,11 +25,32 @@ class MainController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            var_dump($data);
+            $session->set('contactInfo', $data);
+            return $this->redirectToRoute('generate');
         }
 
         return $this->render('main/index.html.twig', [
             'form'=>$form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/generate", name="generate")
+     * @param Session $session
+     * @return Response
+     */
+    public function generatePdf(Session $session)
+    {
+        if (empty($session->get('contactInfo')))
+        {
+            return $this->redirectToRoute('homepage');
+        }
+
+        $data = $session->get('contactInfo');
+
+        return $this->render('main/generate.html.twig', [
+            'data'=> $data
+        ]);
+
     }
 }
